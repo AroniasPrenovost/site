a13516f9d5709aad04fa767654998ec0a46d6773
Aron Jon Prenovost <Aroniascoder@gmail.com>
2023-02-07 07:51:44 -0800

    updating description

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 const About = () => {
   return (
     <div className='info-container'>
-      <p>I'm a software engineer interested in full stack development, ecommerce, and microservices.<br></br><br></br> Check out my <a href='#projects' title='Side projects'>side projects</a>.</p>
+      <p>I'm a software engineer interested in full stack development, ecommerce, and microservices.</p>
     </div>
   );
 };
